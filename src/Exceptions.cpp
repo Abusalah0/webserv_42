3718ec0f948d1a118a761b7dfa8a17025ce7275f
abusalah0 <109g83@gmail.com>
2025-08-16 12:35:46 +0300

    default server exception definition

--- src/Exceptions.cpp
+++ src/Exceptions.cpp
@@ -4,3 +4,8 @@ const char* WebservExceptions::FileOpenFailure::what() const throw()
 {
     return "Opening file failed!";
 }
+
+const char* WebservExceptions::ADefaultServerAlreadyExists::what() const throw()
+{
+    return "A default server already exists!";
+}
