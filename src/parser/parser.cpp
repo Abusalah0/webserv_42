cae9d82b333a69da37a2b8cf4eb9018b2bb4f276
stacks-of-data <coder64bit@gmail.com>
2025-09-08 22:54:29 +0300

    Zombie Fix

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -6,7 +6,7 @@
 /*   By: amsaleh <amsaleh@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 22:00:55 by abdsalah          #+#    #+#             */
-/*   Updated: 2025/09/07 22:21:34 by amsaleh          ###   ########.fr       */
+/*   Updated: 2025/09/08 22:53:10 by amsaleh          ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -37,7 +37,8 @@ void parser(const std::vector<t_token> &tokens, ServerContainer &serverContainer
     parse_baseblock(tokens, baseBlock, pos);
     // parse server blocks
     parse_servers(tokens, serverContainer, baseBlock, pos);
-
+	if (pos < tokens.size())
+        throw_parse_error("Unexpected tokens after http block");
     //print servercontainer for debugging
     // print_server_container(serverContainer);
 }
